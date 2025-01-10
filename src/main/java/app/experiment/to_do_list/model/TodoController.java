7c1b676264646b03342c7e49c61029203d220295
techieyuvi <yuvaraj.k1782@gmail.com>
2025-01-10 20:09:28 +0530

    Updating the CRUD Operations

--- src/main/java/app/experiment/to_do_list/model/TodoController.java
+++ src/main/java/app/experiment/to_do_list/model/TodoController.java
@@ -1,11 +1,10 @@
 package app.experiment.to_do_list.model;
 
 import app.experiment.to_do_list.entity.Todo;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,6 +26,33 @@ public class TodoController {
         Optional<Todo> todo = todoService.getTodoById(id);
         return todo.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @GetMapping
+    public ResponseEntity<Todo> CreateTodo(@RequestBody Todo todo){
+        Todo createdTodo =todoService.addTodo(todo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTodo);
+    }
+
+    @PutMapping
+    public ResponseEntity<Todo> updateToDo(@PathVariable Long id,Todo todo){
+        Todo updateTodo = todoService.updateTodo(id,todo );
+        if(updateTodo != null){
+          return ResponseEntity.ok(updateTodo);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    public ResponseEntity <Todo> deleteToDo( Long id,Todo todo){
+        todoService.deleteTodo(id);
+        return  ResponseEntity.noContent().build();
+    }
+
+
+
+
+
+
+
 
 
 
